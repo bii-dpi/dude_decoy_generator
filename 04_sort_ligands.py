73fb478d29bcd533d7b2bdc60d18363c73642907
bii-dpi <>
2021-12-08 14:38:09 +0800

    Revised 04 to save memory

--- 04_sort_ligands.py
+++ 04_sort_ligands.py
@@ -1,6 +1,6 @@
 import numpy as np
-
 import pandas as pd
+
 from os import remove
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor
@@ -12,12 +12,19 @@ COLUMNS = ("smiles", "mol_wt", "logp",
 all_zinc_df = pd.read_csv("data/all_zinc_w_prop.csv",
                           names=COLUMNS,
                           dtype={"smiles": str, "mol_wt": np.float32,
-                                 "logp": np.float32, "num_rotatable": int,
-                                 "num_hba": int, "num_hbd": int, "net_charge": int})
+                                 "logp": np.float32, "num_rotatable": np.int16,
+                                 "num_hba": np.int16, "num_hbd": np.int16,
+                                 "net_charge": np.int16})
 print(all_zinc_df.columns)
 
+np.save("data/all_zinc_smiles_ref.npy", all_zinc_df[["smiles"]].to_numpy())
+
 for column in tqdm(COLUMNS[1:]):
-    all_zinc_df[["smiles", column]].\
-        sort_values(by=column).\
-        to_csv(f"data/all_zinc_w_prop_{column}.csv", index=False)
+    smiles_reorder = all_zinc_df.loc[:, column].argsort().to_numpy()
+    np.save(f"data/sorted/{column}_smiles_reorder.npy",
+            smiles_reorder)
+
+    sorted_vals = all_zinc_df.loc[:, column].iloc[smiles_reorder]
+    np.save(f"data/sorted/{column}_values.npy",
+            sorted_vals)
 
