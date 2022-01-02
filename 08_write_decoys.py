4b8e961116aef1607f0e12987c5786fb439e333a
bii-dpi <>
2022-01-02 16:23:52 +0000

    Cleaned 08

--- 08_write_decoys.py
+++ 08_write_decoys.py
@@ -1,7 +1,5 @@
-"""Write decoys from qualifying candidates to disk for the referenced job."""
+"""Write decoys from qualifying candidates to disk."""
 
-import os
-import pickle
 import argparse
 import numpy as np
 import pandas as pd
@@ -11,10 +9,31 @@ from collections import Counter, defaultdict
 
 print("Selecting and writing decoys for each active...")
 
-np.random.seed(12345)
 
 ZINC_SMILES = np.load("data/all_zinc_smiles_ref.npy",
                       allow_pickle=True).flatten()
+
+
+def write_decoys(path, dict_):
+    """Write decoys in the input dictionary to path."""
+
+    to_write = []
+    for active_smiles, decoy_list in dict_.items():
+        for decoy_smiles in decoy_list:
+            to_write.append(f"{active_smiles} {decoy_smiles}")
+
+    with open(path, "w") as f:
+        f.write("\n".join(to_write))
+
+
+def make_percentage(freq, counts_sum):
+    """Get percentage as a pretty string."""
+
+    perc = (freq * 100) // counts_sum
+    if perc < 1:
+        perc = "<1"
+
+    return f"{perc}%"
 
 
 if __name__ == "__main__":
@@ -22,12 +41,13 @@ if __name__ == "__main__":
     parser.add_argument("job_name",
                         help="Job name to be used to load the decoy candidates.")
     parser.add_argument("-o", default=None,
-                        help="Decoy file output path. Default: data/{job_name}_decoys")
+                        help=("Decoy file output path; do not end it with an extension."
+                              " Default: data/\{job_name\}_decoys"))
     args = vars(parser.parse_args())
 
     output_path = args["o"]
     if not output_path:
-        output_path = f"data/{job_name}_decoys"
+        output_path = f"data/{args['job_name']}_decoys"
         print(f"No output path supplied; writing decoys to {output_path}")
 
     print(f"[1/3] Loading decoy candidate dictionary...")
@@ -44,6 +64,7 @@ if __name__ == "__main__":
     qualifying_candidates = dict(zip(qualifying_candidates,
                                      [[] for _ in qualifying_candidates]))
 
+    print("Filtering candidates")
     for smiles_pair in progressbar(candidate_dict):
         candidate_dict[smiles_pair] = \
             [curr_smiles for curr_smiles in candidate_dict[smiles_pair]
@@ -54,45 +75,55 @@ if __name__ == "__main__":
                       for smiles_pair, candidate_smiles in candidate_dict.items()
                       if len(candidate_smiles)}
 
-    print(f"[2/3] Setting candidate priority by its duplicatedness...")
+    # Consolidate candidates across the protonated states of each active.
+    print("Consolidating candidates by active")
+    decoys_dict = defaultdict(set)
+    for smiles_pair, candidate_smiles in progressbar(candidate_dict.items()):
+        decoys_dict[smiles_pair.split("_")[0]].update(candidate_smiles)
+
+    print(f"[2/3] Assigning decoys to actives by duplicatedness...")
     # Calculate candidate counts.
     all_qualified = [smiles for candidate_smiles in candidate_dict.values()
                      for smiles in candidate_smiles]
     candidate_counts = Counter(all_qualified)
-    sorting_key = lambda smiles: candidate_counts[smiles]
 
-    for smiles_pair, candidate_smiles in progressbar(candidate_dict.items()):
-        candidate_dict[smiles_pair] = sorted(candidate_smiles, key=sorting_key)
-
-    actives_dict = defaultdict(set)
     failed_actives = []
-    for smiles_pair, candidate_smiles in progressbar(candidate_dict.items()):
-        actives_dict[smiles_pair.split("_")[0]].update(candidate_smiles)
-
-    for active_smiles, candidate_smiles in progressbar(actives_dict.items()):
-        if len(candidate_smiles) < 50:
+    for active_smiles, candidate_smiles_set in progressbar(decoys_dict.items()):
+        if len(candidate_smiles_set) < 50:
             failed_actives.append(active_smiles)
-        else:
-            actives_dict[active_smiles] = \
-                sorted(list(candidate_smiles), key=sorting_key)[:50]
 
-    actives_dict = {active_smiles: candidate_smiles
-                    for active_smiles, candidate_smiles in actives_dict.items()
-                    if len(candidate_smiles) >= 50}
+        decoys_dict[active_smiles] = \
+            sorted(list(candidate_smiles_set),
+                   key=lambda smiles: candidate_counts[smiles])[:50]
 
-    print(len(failed_actives), "failed")
-    # Need to save these elsewhere and say that I am, and where.
+    if len(failed_actives):
+        failed_output_path = f"{output_path}_failed"
+        print(f"{len(failed_actives)}/{len(decoys_dict)} "
+              f"({make_percentage(len(failed_actives), len(decoys_dict))}) "
+              f"actives have fewer than 50 assignable decoys; writing their "
+              f"decoys to a separate file: {failed_output_path}")
+        failed_dict = dict()
+        for failed_active in failed_actives:
+            failed_dict[failed_active] = decoys_dict[failed_active]
+            del decoys_dict[failed_active]
 
-    print(f"[4/4] Writing decoys...")
-    to_write = []
-    for active_smiles, decoy_list in actives_dict.items():
-        for decoy_smiles in decoy_list:
-            to_write.append(f"{active_smiles} {decoy_smiles}")
+    # Calculate assigned decoy statistics.
+    all_decoys = [smiles for decoy_smiles_list in decoys_dict.values()
+                  for smiles in decoy_smiles_list]
+    decoy_counts = Counter(list(Counter(all_decoys).values()))
+    counts_sum = np.sum(list(decoy_counts.values()))
+    decoy_props = {count: make_percentage(freq, counts_sum)
+                   for count, freq in decoy_counts.items()}
+    pretty_props = ""
+    for count in sorted(list(decoy_props.keys())):
+        pretty_props += f"Assigned to {count} actives: {decoy_props[count]}\n"
 
-    with open(output_path, "w") as f:
-        f.write("\n".join(to_write))
+    print(f"{len(set(all_decoys))} unique decoys assigned")
+    print(pretty_props[:-1])
 
-    # Need to write stats on duplication.
+    print(f"[3/3] Writing decoys...")
+    write_decoys(output_path, decoys_dict)
+    write_decoys(failed_output_path, failed_dict)
 
     print("Done.")
 
--- README.md
+++ README.md
@@ -16,5 +16,6 @@ Credit: [Dimorphite-DL](https://durrantlab.pitt.edu/dimorphite-dl/) is used to g
 
 TODO:
 1. Add a setup.py to do 00-04, and rename 05-07.
-2. Clean 02, 06, 07.
+2. Clean 06.
+3. Enhance readme.
 
